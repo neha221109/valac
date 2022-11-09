db3392824e3148d256d9a02a424c6ffba0ff810c
neha221109 <117840055+neha221109@users.noreply.github.com>
2022-11-09 20:20:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function showPicture(){
+  document.getElementById("girlpic1").src="valac.webp";
+document.getElementById("girlpic1").style.height="800px";
+document.getElementById("girlpic1").syle.width="1000px";
+document.getElementById("audio1").play();
+}
